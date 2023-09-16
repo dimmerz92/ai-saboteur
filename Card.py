6126f47c48be188a37814fcb669820d8249ec6c5
Andrew <andrew.weymes@outlook.com>
2023-09-16 20:51:22 +0800

    add gold attribute

--- Card.py
+++ Card.py
@@ -2,11 +2,12 @@ import constants as C
 from assets import IMG_PATH
 
 class Card:
-    def __init__(self, name, card_type, directions):
+    def __init__(self, name, card_type, directions, gold = False):
         self.name = name
         self.type = card_type
         self.directions = directions
-        self.img = self._set_img(name)
+        self.gold = gold
+        self.img = self._set_img(name, gold)
 
-    def _set_img(self, name):
-        return IMG_PATH[name]
+    def _set_img(self, name, gold):
+        return IMG_PATH[name] + ("_GOLD" if gold else "")
